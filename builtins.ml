bad0a9d0adf3cdc300d9e89259f0b0a4b0480eb8
Shuqi Chen <shuqichen0208@gmail.com>
2019-04-17 15:31:26 -0400

    added list_append declaration

--- builtins.ml
+++ builtins.ml
@@ -23,6 +23,6 @@ let builtins = [
 
   (* List functions *)
   ("list_init", SFunc({sparam_typs= []; sreturn_typ = SList (SAny) }));
-  ("list_init", SFunc({sparam_typs= [SList (SAny); SAny]; sreturn_typ = SListElement (SAny) }));
+  ("list_append", SFunc({sparam_typs= [SList (SAny); SAny]; sreturn_typ = SListElement (SAny) }));
 
 ]
